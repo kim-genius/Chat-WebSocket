3d63391b3203e1af7d67fa2d55ea2db0092f1276
김영재 <dudwodla123@gmail.com>
2023-06-29 17:01:18 +0900

    웹소켓 재연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,57 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 const App = () => {
-  const ws = new WebSocket("ws://localhost:8001/'");
+  // useRef로 관리하겠습니다
+  const webSocket = useRef<WebSocket | null>(null);
 
   useEffect(() => {
-    (() => {
-      ws.onopen = (e) => {
-        console.log("success");
+    // 재연결을 할지 할지 구분 용도
+    let isReconnect = true;
+
+    // 연결 로직
+    const connectWebSocket = () => {
+      webSocket.current = new WebSocket("ws://localhost:8001/");
+
+      const onConnect = async () => {
+        console.log("연결성공");
       };
 
-      ws.onmessage = (e) => {
-        console.log("received message", e);
+      const onReceive = async (e: any) => {
+        console.log("메시지 수신", e);
       };
 
-      ws.onclose = (e) => {
-        console.log("close", e);
+      const onDisconnect = () => {
+        console.log("연결종료");
+        reconnectWebSocket(isReconnect);
       };
-    })();
+
+      webSocket.current.onopen = onConnect;
+      webSocket.current.onmessage = onReceive;
+      webSocket.current.onclose = onDisconnect;
+    };
+
+    // 재연결 로직
+    const reconnectWebSocket = (_isReconnect: boolean) => {
+      if (!_isReconnect) return;
+      console.log("재연결");
+      connectWebSocket();
+    };
+
+    connectWebSocket();
+
+    return () => {
+      isReconnect = false;
+      webSocket.current?.close?.();
+      webSocket.current = null;
+    };
   }, []);
 
   const onSendMessage = () => {
-    ws.send("hi");
+    webSocket.current?.send("hi");
   };
 
   const onCloseWS = () => {
-    ws.close(4000, "그냥");
+    webSocket.current?.close(4000, "그냥");
   };
 
   return (
